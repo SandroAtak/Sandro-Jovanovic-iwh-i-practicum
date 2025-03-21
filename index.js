e00bdbba49b26656504b24028f41f906e97a52ce
SandroAtak <sandro@atakinteractive.com>
2025-03-21 16:42:20 +0100

    Route one homepage get

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
+const path = require('path');
 const app = express();
 
 app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
-
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
+const HUBSPOT_OBJECT_TYPE = process.env.HUBSPOT_OBJECT_TYPE;
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+app.get("/", async (req, res) => {
+    const endpointUrl = `https://api.hubapi.com/crm/v3/objects/${HUBSPOT_OBJECT_TYPE}?properties=name,instrument,birth_year`;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    const params = {
+        properties: ['name', 'instrument', 'birth_year'] 
+      }
+    try {
+        const resp = await axios.get(endpointUrl, { headers, params });
+        const data = resp.data.results;
+        res.render('homepage', { title: 'Beatles | HubSpot APIs', data }); 
+    } catch (error) {
+        console.error(error);
+    }
+});
+
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
